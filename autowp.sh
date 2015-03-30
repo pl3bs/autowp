09438e6c7fc5e253452a956fa42698e008db4461
pl3bs <brandonchapman@gmx.com>
2015-03-29 21:44:32 -0500

    Update autowp.sh

--- autowp.sh
+++ autowp.sh
@@ -1,4 +1,4 @@
 sudo apt-get update;
-echo | sudo apt-get install nginx mysql-server php5-fpm php5-mysql -y;
+echo | sudo apt-get install nginx mysql-server php5-fpm php5-mysql;
 sudo mysql_install_db;
 sudo mysql_secure_installation;
