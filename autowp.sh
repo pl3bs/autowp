ad4b75ca31bc4b41d6561069e4671475ea26a326
pl3bs <brandonchapman@gmx.com>
2015-03-30 14:15:54 -0500

    Update autowp.sh

--- autowp.sh
+++ autowp.sh
@@ -16,7 +16,7 @@ sudo chown -R www-data:www-data /var/www/wordpress/;
 #install nginx & php
 cd /tmp;
 apt-get update;
-apt-get install apache2 libapache2-mod-php5 curl php5-mysql php5-gd libssh2-php -y;
+apt-get install apache2 libapache2-mod-php5 php5-mysql php5-gd libssh2-php -y;
 
 #configure nginx for wordpress
 
