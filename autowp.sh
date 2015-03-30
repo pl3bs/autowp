ed15315106f990e40ef0f3a4b261168764d224a4
pl3bs <brandonchapman@gmx.com>
2015-03-30 00:43:17 -0500

    Update autowp.sh

--- autowp.sh
+++ autowp.sh
@@ -1,12 +1,14 @@
 #install mysql automagically
 
 apt-get update;
-read -p "Enter MySQL Root Password " psss;
-echo mysql-server mysql-server/root_password password "$psss" | sudo debconf-set-selections;
-echo mysql-server mysql-server/root_password_again password "$psss" | sudo debconf-set-selections;
+read -p "Enter MySQL Root Password " sqlr;
+echo mysql-server mysql-server/root_password password "$sqlr" | sudo debconf-set-selections;
+echo mysql-server mysql-server/root_password_again password "$sqlr" | sudo debconf-set-selections;
 apt-get install mysql-server -y;
 sudo mysql_install_db;
-printf "%s\n\nn\nY\nY\nY\nY'" "$psss" | mysql_secure_installation
+printf "%s\n\nn\nY\nY\nY\nY" "$sqlr" | mysql_secure_installation;
+read -p "Enter Wordpress Database User " user;
+printf "CREATE USER wordpressuser@localhost IDENTIFIED BY %s";\n "$user" | mysql -u root --password=$user";
 
 
 
