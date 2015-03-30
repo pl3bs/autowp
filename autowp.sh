2821869217a658aaea83ac8cdf9ac5ba4012c4e6
pl3bs <brandonchapman@gmx.com>
2015-03-30 03:04:14 -0500

    Update autowp.sh

--- autowp.sh
+++ autowp.sh
@@ -1,3 +1,7 @@
+#install nginx & php
+
+apt-get install nginx php5-fpm php5-mysql php5-gd libssh2-php -y;
+
 #install mysql automagically
 
 apt-get update;
@@ -11,8 +15,17 @@ read -p "Enter Wordpress Database User " user;
 printf "CREATE DATABASE wordpress;\nCREATE USER wordpressuser@localhost IDENTIFIED BY '%s';\nGRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost;\nFLUSH PRIVILEGES;\nexit; "$user" | mysql -u root --password="$sqlr";
 
 
+#get wordpress
 
+wget http://wordpress.org/latest.tar.gz;
+tar xzvf latest.tar.gz;
+cd wordpress;
+cp wp-config-sample.php wp-config.php;
+sed -i "/^define('DB_NAME'/ s/database_name_here');$/wordpress');/g" wp-config.php;
+sed -i "/^define('DB_USER'/ s/username_here');$/wordpressuser');/g" wp-config.php;
+sed -i "/^define('DB_PASSWORD'/ s/password_here');$/"$user"');/g" wp-config.php;
 
-
-#install nginx & php
-#apt-get install nginx php5-fpm php5-mysql -y;
+mkdir /var/www/wordpress;
+sudo rsync -avP ~/wordpress/ /var/www/html/;
+mkdir /var/www/wordpress/uploads;
+sudo chown -R www-data:www-data /var/www/wordpress/*;
