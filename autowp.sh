628dc8a955cb38847bdcc1ea81b398ab9f97f429
pl3bs <brandonchapman@gmx.com>
2015-03-29 22:50:18 -0500

    Update autowp.sh

--- autowp.sh
+++ autowp.sh
@@ -1,4 +1,17 @@
-sudo apt-get update;
-echo | sudo apt-get install nginx mysql-server php5-fpm php5-mysql;
+#install mysql
+
+apt-get update;
+read -p "Enter MySQL Root Password " psss;
+echo mysql-server mysql-server/root_password password Y | sudo debconf-set-selections;
+echo mysql-server mysql-server/root_password_again password Y | sudo debconf-set-selections;
+apt-get install mysql-server -y;
 sudo mysql_install_db;
-sudo mysql_secure_installation;
+yes Y | mysql_secure_installation;
+yes Y | mysql -u root -p;
+
+
+
+
+
+#install nginx & php
+#apt-get install nginx php5-fpm php5-mysql -y;
