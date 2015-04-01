573b879222a8e7ffc48ef185961d4eab48f54129
pl3bs <brandonchapman@gmx.com>
2015-03-31 23:30:36 -0500

    Update disc_autowp.sh

--- disc_autowp.sh
+++ disc_autowp.sh
@@ -28,7 +28,7 @@ rm /var/www/html/index.html;
 
 #configure nginx for wordpress
 
-wget https://raw.githubusercontent.com/pl3bs/autowp/testing/disc_apache-proxy.conf;
+wget https://raw.githubusercontent.com/pl3bs/autowp/testing/apache-proxy.conf;
 mv apache-proxy.conf /etc/nginx/conf.d/apache-proxy.conf;
 rm /etc/nginx/conf.d/000-default.conf;
 
